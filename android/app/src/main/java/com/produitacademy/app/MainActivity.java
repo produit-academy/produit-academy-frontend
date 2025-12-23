8bfbcb911fd96ef5283b06a8667f7e79bbeaf849
produit-academy <techproduit6@gmail.com>
2025-12-23 15:36:26 +0530

    App Name Change

--- android/app/src/main/java/com/produitacademy/app/MainActivity.java
+++ android/app/src/main/java/com/produitacademy/app/MainActivity.java
@@ -6,9 +6,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Desktop view forced settings removed for responsive mobile layout
         if (getBridge() != null && getBridge().getWebView() != null) {
-            // Keep any essential configs here if needed in future
         }
     }
 }
