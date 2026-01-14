365d5b240d111142b57d2db87a0eb2791252d0fd
ras-al <rasalkpk66@gmail.com>
2026-01-15 02:02:55 +0530

    url change

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
   const [branches, setBranches] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/branches/')
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/branches/`)
       .then(res => res.json())
       .then(data => setBranches(data))
       .catch(err => console.error("Failed to fetch branches", err));
@@ -60,7 +60,7 @@ export default function Home() {
     setIsSubmitting(true);
     setSubmitResult({ type: '', message: '' });
     try {
-      const res = await fetch('http://127.0.0.1:8000/api/contact/', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contact/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
